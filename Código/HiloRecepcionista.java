8e7fba6d59b9f9834e33b383d7497e47e04e20d0
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2025-06-04 17:37:34 -0300

    Add files via upload

---
+++ Código/HiloRecepcionista.java
@@ -0,0 +1,50 @@
+import java.util.concurrent.Semaphore;
+
+public class HiloRecepcionista extends Thread {
+    Semaphore disponible;
+
+    public HiloRecepcionista(String identificador) {
+        super(identificador);
+        disponible = new Semaphore(1);
+    }
+
+    // IMPLEMENTACIONES PROVISORIAS, BURDAS, DEL COMPORTAMIENTO GENERAL
+    public boolean registrarPaciente(Thread paciente) {
+        if (/* Condición que tenga que cumplirse para registrar a un paciente*/ true) {
+            try {
+                disponible.acquire();   // Equivalente a wait()
+                Thread.sleep(/* Tiempo que dure la consulta */ 500);
+                System.out.println("[Recepcionista " +this.getId()+ "]:" + "Paciente registrado: " + paciente.getName());
+            }
+            catch (InterruptedException e) {
+                System.out.println("Error por interrupción");
+            }
+            finally {
+                disponible.release();   // Equivalente a signal()
+            }
+            return true;
+        }
+        // Si no se cumple la condición para registrar al paciente
+        System.out.println("[Odontólogo " +this.getId()+ "]:" + "No se pudo atender al paciente: " + paciente.getName());
+        return false;
+    }
+
+    public void finDeTurno(int escalaReloj, int horasDescanso) {
+        // Si no está registrando a alguien (está disponible),
+        // Procede a tomarse el palo y dormir por el resto del día
+        try {
+            disponible.acquire();   // wait() a que esté disponible	
+            Thread.sleep(escalaReloj * horasDescanso * 1000);
+        } 
+        catch (InterruptedException e) {
+            System.out.println("Error por interrupción");
+        } 
+        finally {
+            disponible.release();   // signal() para que pueda volver a atender pacientes
+        }
+    }
+
+    public void run() {
+       // Implementación
+    } 
+}
---
+++ Código/Planificador.java
@@ -0,0 +1,8 @@
+public class Planificador {
+    // Semáforos, colas y otros atributos
+
+    // Implementación ejecutable
+    public static void main(String[] args) {
+        System.out.println("Planificador iniciado.");
+    }
+}
